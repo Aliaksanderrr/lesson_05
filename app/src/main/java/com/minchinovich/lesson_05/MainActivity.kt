07cf9d55a96b481b2c818552f41462e8c0dd4e07
Aliaksanderrr <midas1989.cc@gmail.com>
2021-10-07 20:46:18 +0300

    after code review

--- app/src/main/java/com/minchinovich/lesson_05/MainActivity.kt
+++ app/src/main/java/com/minchinovich/lesson_05/MainActivity.kt
@@ -1,29 +1,29 @@
 package com.minchinovich.lesson_05
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.minchinovich.lesson_05.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toastText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
-    override fun onStart() {
-        super.onStart()
+        toastText = getString(R.string.exception_toast_message)
+
         binding.sumButton.setOnClickListener {
-            if (binding.firstNumberEdit.text.isNotEmpty() && binding.secondNumberEdit.text.isNotEmpty()){
-                var arg1 = binding.firstNumberEdit.text.toString().toIntOrNull() ?: 0
-                var arg2 = binding.secondNumberEdit.text.toString().toIntOrNull() ?: 0
+            val arg1 = binding.firstNumberEdit.text.toString().toIntOrNull()
+            val arg2 = binding.secondNumberEdit.text.toString().toIntOrNull()
+            if (arg1 != null && arg2 != null) {
                 binding.sumResultView.text = (arg1 + arg2).toString()
             } else {
-                Toast.makeText(this, getString(R.string.exception_toast_message), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show()
             }
         }
     }
